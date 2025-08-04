7243c4d433cafc06a00dbf531c862c9eb298aa93
Yael Badillo <yael.badillos@gmail.com>
2025-08-03 21:54:41 -0600

    feat: add Icons

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,17 @@
 export { Button } from './Button';
 export { Spinner } from './Spinner';
+export { Input } from './Input';
+export { Textarea } from './Textarea';
+export { Checkbox } from './Checkbox';
+export { Toggle } from './Toggle';
+export { RadioButton, RadioGroup } from './RadioButton';
+
+export { MailIcon, EyeIcon, EyeOffIcon, CheckIcon } from './icons';
 
 export type { ButtonProps } from './Button';
 export type { SpinnerProps } from './Spinner';
+export type { InputProps } from './Input';
+export type { TextareaProps } from './Textarea';
+export type { CheckboxProps } from './Checkbox';
+export type { ToggleProps } from './Toggle';
+export type { RadioButtonProps, RadioGroupProps } from './RadioButton';
